b27780a03d373530050a93ee7059e1cbc0665f73
Austin Patton <SCS374Group1@gmail.com>
2023-03-31 00:07:55 -0400

    Initial Commit

---
+++ SCS 141 HW3 Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCS 141 HW3 Light
+//
+//  Created by Austin Patton on 3/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
